c181f0b77bd3918e4182f5a481df252348524506
Erick <takashi.onimaru@gmail.com>
2020-09-21 18:10:33 -0400

    Arranque de dos comandos

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@ const key = 'AimeGabrielaSophia';
 
 const config = {
     dbUrl: process.env.BD_URL || 'mongodb+srv://' + dbUser + ':' + dbPass + '@cluster0.mtjcn.mongodb.net/' + dbName + '?retryWrites=true&w=majority',
-    port: process.env.PORT || 3000,
+    port: process.env.PORT || 4000,
     host: process.env.HOST || 'http://localhost',
     JWT_KEY: process.env.JWT_KEY || key,
     publicRoute: process.env.PUBLIC_ROUTE || '/public',
