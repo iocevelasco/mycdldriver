02fe586809e365fedb326d3f3592ef6580d91950
Erick <takashi.onimaru@gmail.com>
2020-09-25 17:45:49 -0400

    Actualizado el enrutamiento en master

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const next = require('next');
-const path = require('path');
-const url = require('url');
 const cluster = require('cluster');
 const numCPUs = require('os').cpus().length;
 const router_front = require("./network/routes"); 
@@ -25,7 +23,6 @@ if (!dev && cluster.isMaster) {
 
 } else {
   const nextApp = next({ dir: '.', dev });
-  const nextHandler = nextApp.getRequestHandler();
 
   nextApp.prepare()
     .then(() => {
@@ -44,31 +41,9 @@ if (!dev && cluster.isMaster) {
           res.redirect("https://" + req.headers.host + req.url);
         });
       }
-      
-      // Static files
-      // https://github.com/zeit/next.js/tree/4.2.3#user-content-static-file-serving-eg-images
-      server.use('/static', express.static(path.join(__dirname, 'static'), {
-        maxAge: dev ? '0' : '365d'
-      }));
-    
-      // Example server-side routing
-      server.get('/a', (req, res) => {
-        return nextApp.render(req, res, '/b', req.query)
-      })
 
-      // Example server-side routing
-      server.get('/b', (req, res) => {
-        return nextApp.render(req, res, '/a', req.query)
-      })
-
-      // Default catch-all renders Next app
-      server.get('*', (req, res) => {
-        // res.set({
-        //   'Cache-Control': 'public, max-age=3600'
-        // });
-        const parsedUrl = url.parse(req.url, true);
-        nextHandler(req, res, parsedUrl);
-      });
+      router_api(server);
+      router_front(server, dev, nextApp);
 
       server.listen(port, (err) => {
         if (err) throw err;
