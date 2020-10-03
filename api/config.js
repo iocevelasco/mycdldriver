caf45005869036910cb1850ba3da7edd461ba31d
Erick <takashi.onimaru@gmail.com>
2020-10-03 01:20:34 -0400

    Modificado el callback

--- api/config.js
+++ api/config.js
@@ -15,8 +15,8 @@ const config = {
         domain: process.env.AUTH0_DOMAIN || 'dev-8zm642k2.us.auth0.com',
         clientID: process.env.AUTH0_CLIENT_ID || 'pLRsn8o0a1uyBULqVk62sF3LcMMHfahr',
         clientSecret: process.env.AUTH0_CLIENT_SECRET || '5OLosllM-ZDwwJ4aU2u5HxhAAFzxGKD6DBl-XRZc-Vzmty-8cMm4UdllVw1sjBqN',
-        callbackURL: process.env.AUTH0_CALLBACK_URL || 'https://www.mycdldriver411.com/callback',
-        baseURL: process.env.BASE_URL || 'https://www.mycdldriver411.com'
+        callbackURL: process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback',
+        baseURL: process.env.BASE_URL || 'http://localhost:3000'
     }
 };
 
--- auth-routes.js
+++ auth-routes.js
@@ -11,7 +11,7 @@ router.get("/login", passport.authenticate("auth0", {
 router.get("/callback", (req, res, next) => {
   passport.authenticate("auth0",  (err, user) => {
     if (err) return next(err);
-    if (!user) return res.redirect("/login");
+    if (!user) return res.redirect("/");
     req.logIn(user, (err) => {
       if (err) return next(err);
       res.redirect("/");
