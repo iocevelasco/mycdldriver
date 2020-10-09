36df9561b4b628f87c4c6bac8cc6ba6229a1cedd
Erick <takashi.onimaru@gmail.com>
2020-10-09 12:25:06 -0400

    Actualizadas las variables de entorno

--- api/config.js
+++ api/config.js
@@ -13,14 +13,14 @@ const config = {
     dev: process.env.NODE_ENV !== 'production',
     oauth:  {
         google: {
-            clientID: process.env.OAUTH2_CLIENT_ID || '762087274564-vhbjmlqm5vc8qmc9slkd8tjtkh447dut.apps.googleusercontent.com',
-            clientSecret: process.env.OAUTH2_CLIENT_SECRET || '2EjAZAVFLJFjLLBxBS2Z0d5y',
-            callbackURL: process.env.OAUTH2_CALLBACK || 'http://localhost:3000/auth/google/callback'
+            clientID: process.env.GOOGLE_CLIENT_ID || '762087274564-vhbjmlqm5vc8qmc9slkd8tjtkh447dut.apps.googleusercontent.com',
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET || '2EjAZAVFLJFjLLBxBS2Z0d5y',
+            callbackURL: process.env.GOOGLE_CALLBACK || 'http://localhost:3000/auth/google/callback'
         },
         facebook: {
-            clientID: process.env.OAUTH2_CLIENT_ID || '371266210898392',
-            clientSecret: process.env.OAUTH2_CLIENT_SECRET || '50eeb472813974f76e27e299ab859de4',
-            callbackURL: process.env.OAUTH2_CALLBACK || 'http://localhost:3000/auth/facebook/callback'
+            clientID: process.env.FACEBOOK_CLIENT_ID || '371266210898392',
+            clientSecret: process.env.FACEBOOK_CLIENT_SECRET || '50eeb472813974f76e27e299ab859de4',
+            callbackURL: process.env.FACEBOOK_CALLBACK || 'http://localhost:3000/auth/facebook/callback'
         }
     }
 };
