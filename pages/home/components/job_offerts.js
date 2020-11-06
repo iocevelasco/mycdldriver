f66d2c8a25c0a66b993cc94a5afaa75dfd2cd753
iovelasco <ioce.velasco@mercadolibre.com>
2020-11-05 23:50:00 -0300

    listado agregado de jobs

--- pages/home/components/job_offerts.js
+++ pages/home/components/job_offerts.js
@@ -3,6 +3,7 @@ import React from 'react';
 import {
   Row,
   Col,
+  List,
   Card,
   Avatar,
   Typography,
@@ -15,50 +16,64 @@ import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/ico
 const { Text, Title } = Typography
 const { Meta } = Card;
 
-const JobListComp = ({ title, image, description, address, date, postion_id }) => {
+const JobListComp = ({ jobs }) => {
   return (
     <>
-   <Link href={{
-     pathname: '/jobs',
-     query:{
-       id: postion_id
-     }}}>
-      <Card
-        bodyStyle={{
-          padding: 0
-        }}
-        style={{ width: '100%', marginTop: 24, }}>
-        <div className='home--job-offert'>
-          <div className='thumbnails'>
-            <Avatar size={120} src={image} />
-          </div>
-          <div className='job-offert__description'>
-            <div>
-              <Title level={3}> {title} </Title>
-              <div>
-                <Text> Addres </Text>
-                <Text strong> {address} </Text> <Text strong > | </Text>
-                <Text> Date </Text>
-                <Text strong> {date} </Text>
-              </div>
-              <Text> {description} </Text>
-              <div>
-              </div>
-            </div>
-          </div>
-          <div className='action'>
-          <Link
-            href={{
-              pathname: '/jobOffert',
-              query: { id: postion_id },
-              }}
-              >
-               <Button type='primary'> Apply </Button>
-            </Link>
-          </div>
-        </div>
-      </Card>
-      </Link>
+    <List
+      bordered={false}
+      dataSource={jobs}
+      pagination={{
+        onChange: page => {
+          console.log(page);
+        },
+        pageSize: 10,
+      }}
+      renderItem={item => (
+        <List.Item>
+          <Link href={{
+            pathname: '/jobs',
+            query:{
+              id: item._id
+            }}}>
+              <Card
+                bodyStyle={{
+                  padding: 0
+                }}
+                style={{ width: '100%', marginTop: 24, }}>
+                <div className='home--job-offert'>
+                  <div className='thumbnails'>
+                    <Avatar size={120} src={item.image} />
+                  </div>
+                  <div className='job-offert__description'>
+                    <div>
+                      <Title level={3}> {item.title} </Title>
+                      <div>
+                        <Text> Addres </Text>
+                        <Text strong> {item.address} </Text> <Text strong > | </Text>
+                        <Text> Date </Text>
+                        <Text strong> {item.date} </Text>
+                      </div>
+                      <Text> {item.description} </Text>
+                      <div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className='action'>
+                  <Link
+                    href={{
+                      pathname: '/jobOffert',
+                      query: { id: item._id },
+                      }}
+                      >
+                      <Button type='primary'> Apply </Button>
+                    </Link>
+                  </div>
+                </div>
+              </Card>
+              </Link>
+        </List.Item>
+      )}
+     />
     </>
   );
 }
