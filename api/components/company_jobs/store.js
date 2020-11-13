42ee6905bac4c9b949daf007d0b7410a55461cc7
Erick <takashi.onimaru@gmail.com>
2020-11-13 14:06:38 -0400

    Query correction

--- api/components/company_jobs/store.js
+++ api/components/company_jobs/store.js
@@ -71,7 +71,7 @@ function getJobs(filterCompany){
 async function getCustomList(){
     const titles = await JobsModel.find({}).select("title");
     const citys = await JobsModel.find({}).distinct('city');
-    const companys = await ProfileCompany.find().select("tradename");
+    const companys = await ProfileCompany.find({}).select("tradename");
     
     const listado = {
         title: titles,
