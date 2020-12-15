d1eee2a970a17c44b4dfb2f7d2099a5f26482ff4
Erick <takashi.onimaru@gmail.com>
2020-12-15 18:41:19 -0400

    Ajustes de documentacion

--- api/components/user/network.js
+++ api/components/user/network.js
@@ -7,48 +7,133 @@ const auth = require('../../middelware/auth');
 const mailer = require('../../middelware/mailer');
 
 /**
- * @api {get} /user getUserList
+ * @api {get} /user/:type getUserList
  * @apiGroup User
  * @apiVersion 1.0.0
- * @apiSuccess {Object[]} user Datos del usuario registrado
- * @apiSuccess {Id} user._id ID de usuario
- * @apiSuccess {String} user.name Nombre del usuario
- * @apiSuccess {String} user.lastname Apellido del usuario
- * @apiSuccess {String} user.photo Url de la imagen del usuario
- * @apiSuccess {Date} user.date Fecha de registro del usuario
- * @apiSuccess {String} user.email Correo electronico del usuario
- * @apiSuccess {Number} user.typeUser Tipo de usuario (1-> Driver | 2-> Company)
- * @apiSuccess {Number} user.google_id ID de usuario de google cuando se loguea desde esa red social
- * @apiSuccess {Number} user.facebook_id ID de usuario de facebook cuando se loguea desde esa red social
- * @apiSuccess {Object[]} user.driver Si el usuario es Driver (1) aqui estan todos sus datos
- * @apiSuccess {Object[]} user.company Si el usuario es Driver (1) aqui estan todos sus datos
- * @apiSuccess {Object[]} user.tokens Listado de tokens de seguridad registrados por el usuario
- * @apiSuccess {Id} user.tokens._id ID del token
- * @apiSuccess {String} user.tokens.token Token de seguridad
+ * @apiParam {Number} type Tipo de usuario (1-> Driver | 2-> Company)
+ * @apiSuccess {Id} _id ID de usuario
+ * @apiSuccess {String} name Nombre del usuario
+ * @apiSuccess {String} lastname Apellido del usuario
+ * @apiSuccess {String} photo Url de la imagen del usuario
+ * @apiSuccess {Date} date Fecha de registro del usuario
+ * @apiSuccess {String} email Correo electronico del usuario
+ * @apiSuccess {Object[]} driver Si el usuario es Driver (1) aqui estan todos sus datos
+ * @apiSuccess {Id} driver._id Id del driver
+ * @apiSuccess {Number} driver.dln Numero de identificacion de la licencia
+ * @apiSuccess {Date} driver.expDateDln Fecha de expiracion de la licencia
+ * @apiSuccess {String} driver.imageDln Url de la imagen de la licencia
+ * @apiSuccess {Date} driver.birthDate Fecha de cumpleaños del conductor
+ * @apiSuccess {Number} driver.areaCode Codigo de area del telefono
+ * @apiSuccess {String} driver.phoneNumber Numero de telefono
+ * @apiSuccess {Number} driver.sex Sexo del driver (1-> Hombre | 2-> Mujer | 3-> Otro) 
+ * @apiSuccess {String} driver.address Direccion fisica de habitacion 
+ * @apiSuccess {Number} driver.zipCode Zip Code 
+ * @apiSuccess {Number} driver.rating Promedio de puntuacion del driver 
+ * @apiSuccess {String} driver.description Descripcion breve del Driver 
+ * @apiSuccess {Object[]} driver.experience Arreglo de objetos con campos de experiencia 
+ * @apiSuccess {String} driver.experience.name Nombre del campos de experiencia 
+ * @apiSuccess {Boolean} driver.experience.have Indica si posee el campo de experiencia 
+ * @apiSuccess {Number} driver.experience.years Numero de años de experiencia 
+ * @apiSuccess {Object[]} company Si el usuario es Company (2) aqui estan todos sus datos
+ * @apiSuccess {Id} company._id Id de la empresa
+ * @apiSuccess {String} company.tradename Nombre comercial
+ * @apiSuccess {String} company.legalNumber Numero de identificacion de la empresa
+ * @apiSuccess {Number} company.areaCode Codigo de area del telefono
+ * @apiSuccess {String} company.phoneNumber Numero de telefono
+ * @apiSuccess {String} company.address Direccion fisica de la empresa
+ * @apiSuccess {String} company.address2 Direccion segunda linea 
+ * @apiSuccess {String} company.description Descripcion corta de la empresa 
+ * @apiSuccess {String} company.zipCode ZipCode 
+ * @apiSuccess {String} company.logo Url de la imagen de la empresa 
+ * @apiSuccess {String} company.state Estado donde esta ubicada 
+ * @apiSuccess {Id} company.state._id Id del estado 
+ * @apiSuccess {String} company.state.stateName Nombre del estado 
+ * @apiSuccess {String} company.ciudad Ciudad donde esta ubicada 
+ * @apiSuccess {Id} company.ciudad._id Id de la ciudad 
+ * @apiSuccess {String} company.ciudad.stateName Nombre de la ciudad 
  * @apiSuccessExample {json} Ejemplo de respuesta correcta
  * {
-    "error": 0,
-    "mensaje": [
-        {
-        "_id": "5f72aad62f9b9a1c605e2978",
-        "name": "Juan",
-        "lastname": "Perez",
-        "date": "2020-02-10T15:46:51.778Z",
-        "email": "juanperez@gmail.com",
-        "typeUser": 1,
-        "google_id": "107579236887242099879",
-        "facebook_id": "10158714673184036",
-        "driver": "5f8f6780b57fcf31145bb163",
-        "company": "5f8f726442cd1a1498021acf",
-        "photo": "http://localhost:3000./public/files/haHR0E2O68nPpcpx668vEJRYC33hHSritUY0tOIE42nkOyN94saIKDB7QzJ-q-iS.jpg",
-        "tokens": [
+    "data": [
+    {
+        "_id": "5fb5479b949d6a2d98a91945",
+        "name": "Erick",
+        "lastname": "Hernandez",
+        "photo": "https://www.unitecnar.edu.co/sites/default/files/pictures/user_default.png",
+        "email": "takashi@dragonrock.com.ve",
+        "date": "2020-11-18T16:11:07.098Z",
+        "driver": {
+            "_id": "5fb5479a949d6a2d98a91944",
+            "dln": 16436589,
+            "imageDln": "/public/files/KhSaP8-nHTasW2R6rgPZR0kuVijdX3Z9NKo7oa-fHX3JVaBluSxGyBjY9sHM-4qk.jpg",
+            "expDateDln": "2020-11-28T04:00:00.000Z",
+            "birthDate": "2020-11-01T04:00:00.000Z",
+            "areaCode": 414,
+            "phoneNumber": "3168556",
+            "rating": 3,
+            "sex": 1,
+            "experience": [
             {
-            "_id": "5f72aad62f9b9a1c605e2979",
-            "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZjcyYWFkNjJmOWI5YTFjNjA1ZTI5NzgiLCJpYXQiOjE2MDEzNTAzNTh9.yaU-FnWzUKzBvNxAchS0O6sGWoD5GpyC-lWombv4rA8"
+                "years": 4,
+                "_id": "5fd7a053398f454368ccb2be",
+                "name": "Tamk endorsed",
+                "have": true
+            },
+            {
+                "years": 2,
+                "_id": "5fd7a053398f454368ccb2bf",
+                "name": "Hazmat",
+                "have": true
+            },
+            {
+                "years": 0,
+                "_id": "5fd7a053398f454368ccb2c0",
+                "name": "Referred loads",
+                "have": false
+            },
+            {
+                "years": 1,
+                "_id": "5fd7a053398f454368ccb2c1",
+                "name": "Van",
+                "have": true
+            },
+            {
+                "years": 10,
+                "_id": "5fd7a053398f454368ccb2c2",
+                "name": "Car Carrier",
+                "have": true
+            },
+            {
+                "years": 3,
+                "_id": "5fd7a053398f454368ccb2c3",
+                "name": "Flat Bed",
+                "have": true
             }
-        ],
-        "__v": 1
+            ],
+            "address": "Venezuela",
+            "zipCode": "1030",
+            "description": "klhg gh kh fth dr"
+        },
+        "company": {
+            "_id": "5faeb15ef909381520db5c50",
+            "tradename": "Dragonrock",
+            "legalNumber": "1478956327",
+            "areaCode": 414,
+            "phoneNumber": "31685567",
+            "address": "Los Dos Caminos",
+            "address2": "ghfhfh",
+            "description": "Cualquier cosa",
+            "zipCode": "1030",
+            "logo": "/public/files/UWy-arz6QFkscjZpxGfJiuIO1syhP6XDrLmJuzwAZWkwk5HfbcPkhM8240n4CWgT.png",
+            "state": {
+                "_id": "5fc68b66fd5663f19004aca6",
+                "stateName": "California"
+            },
+            "city": {
+                "_id": "5fc6a427fd5663f1900b7813",
+                "cityName": "Floriston"
+            }
         }
+    }
     ]
 }
  * @apiErrorExample {json} List error
@@ -79,8 +164,7 @@ router.get('/:type', function (req, res) {
  * @apiParam {Id} _id ID de usuario
  * @apiSuccessExample {json} Ejemplo de respuesta correcta
  * {
-  "error": 0,
-  "mensaje": "Usuario 5f72ba72f5aa7224d8cc9ee2 eliminado"
+  "data": "Usuario 5f72ba72f5aa7224d8cc9ee2 eliminado"
 }
  * @apiErrorExample {json} List error
  *    HTTP/1.1 500 Internal Server Error
@@ -110,6 +194,7 @@ router.delete('/:id', auth(), function (req, res) {
  * @apiSuccess {Date} date Fecha de registro del usuario
  * @apiSuccess {String} token Token de seguridad
  * @apiSuccess {Object[]} driver Datos del conductor
+ * @apiSuccess {Id} driver._id Id del conductor
  * @apiSuccess {Number} driver.dln Numero de licencia del conductor
  * @apiSuccess {File} driver.imageDln Foto de la licencia del conductor 
  * @apiSuccess {Date} driver.birthDate Fecha de nacimiendo del conductor 
@@ -125,17 +210,24 @@ router.delete('/:id', auth(), function (req, res) {
  * @apiSuccess {String} driver.description Descripcion breve del conductor, habilidades, destrezas, etc... 
  * @apiSuccess {Date} driver.expDateDln Fecha de vencimiento de la licencia 
  * @apiSuccess {Object[]} company Datos de la compañia
+ * @apiSuccess {Id} company._id Id de la empresa
  * @apiSuccess {String} company.tradename Nombre de la compañia 
  * @apiSuccess {String} company.legalNumber Numero de registro de la empresa 
  * @apiSuccess {Number} company.areaCode Codigo de area del telefono 
  * @apiSuccess {Number} company.phoneNumber Numero de telefono 
  * @apiSuccess {File} company.logo Foto del logo de la empresa 
- * @apiSuccess {String} company.address Direccion fisica de habitacion  
+ * @apiSuccess {String} company.address Direccion fisica de empresa 
+ * @apiSuccess {String} company.address2 Direccion linea 2  
  * @apiSuccess {String} company.description Descripcion breve de la empresa
  * @apiSuccess {Number} company.zipCode Zip Code  
+ * @apiSuccess {String} company.state Estado donde esta ubicada 
+ * @apiSuccess {Id} company.state._id Id del estado 
+ * @apiSuccess {String} company.state.stateName Nombre del estado 
+ * @apiSuccess {String} company.ciudad Ciudad donde esta ubicada 
+ * @apiSuccess {Id} company.ciudad._id Id de la ciudad 
+ * @apiSuccess {String} company.ciudad.stateName Nombre de la ciudad 
  * @apiSuccessExample {json} Ejemplo de respuesta correcta
  * {
-  "error": 0,
   "data": {
     _id: 5f8f6781b57fcf31145bb164,
     name: 'Pedro',
@@ -148,6 +240,7 @@ router.delete('/:id', auth(), function (req, res) {
     date: 2020-10-20T22:41:05.496Z,
     token: 'eyJhbGciOiJIUzI19LIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZjhmNjc4MWI1N2ZjZjMxMTQ1YmIxNjQiLCJpYXQiOjE2MDM2NTgzOTB9.GIdlwhPUgpIQkJH61y31U26bdxDChRvK_xGT7Pf-VK0',
     driver: {
+        _id: "5faeb15ef909381520db5c50",
         dln: 14258369,
         imageDln: '/public/files/undefined',
         birthDate: 1988-05-11T00:00:00.000Z,
@@ -199,14 +292,24 @@ router.delete('/:id', auth(), function (req, res) {
         ]
     },
     company: {
+        _id: "5faeb15ef909381520db5c50",
         tradename: 'Lorem Ipsum',
         legalNumber: 'V14258369',
         areaCode: 424,
         phoneNumber: 7845612,
         logo: '/public/files/undefined',
-        address: 'Miranda - Caracas - Venezuela',
+        address: 'Miranda - Caracas',
+        address2: 'Venezuela',
         description: 'Maecenas consectetur velit sit amet lorem auctor ultrices',
-        zipCode: '1031'
+        zipCode: '1031',
+        state: {
+          _id: "5fc68b66fd5663f19004aca6",
+          stateName: "California"
+        },
+        city: {
+          _id: "5fc6a427fd5663f1900b7813",
+          cityName: "Floriston"
+        }
     }
 }
  * @apiErrorExample {json} List error
@@ -230,8 +333,7 @@ router.post('/login', async(req, res) => {
  * @apiHeader {String} token Token de acceso de usuario.
  * @apiSuccessExample {json} Ejemplo de respuesta correcta
  * {
-  "error": 0,
-  "mensaje": "Done"
+  "data": "Done"
 }
  * @apiErrorExample {json} List error
  *    HTTP/1.1 500 Internal Server Error
@@ -255,8 +357,7 @@ router.post('/login', async(req, res) => {
  * @apiHeader {String} token Token de acceso de usuario.
  * @apiSuccessExample {json} Ejemplo de respuesta correcta
  * {
-  "error": 0,
-  "mensaje": "Done"
+  "data": "Done"
 }
  * @apiErrorExample {json} List error
  *    HTTP/1.1 500 Internal Server Error
