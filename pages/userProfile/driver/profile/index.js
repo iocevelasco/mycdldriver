e53cff4509813ab2ca2c1c191d58711fc9c967c7
iovelasco <ioce.velasco@mercadolibre.com>
2020-12-17 12:57:34 -0300

    campos form driver agregados

--- pages/userProfile/driver/profile/index.js
+++ pages/userProfile/driver/profile/index.js
@@ -11,23 +11,23 @@ import { WrapperSection } from 'components/helpers';
 const DriverProfileView = (props) => {
   const stylesWrapper = {
     background: `url('/static/images/bg-routes.jpg')`,
-    paddingTop: 24, 
+    paddingTop: 24,
     paddingBottom: 24,
-    backgroundSize:'contain',
+    backgroundSize: 'contain',
   }
 
-    return (
-      <MainLayout title='Profile'>
-        <Row display='flex' justify='center'>
-          <SideNav currentLocation='0' /> 
-          <Col span={20}>
-            <WrapperSection styles={stylesWrapper} row={16} mt={0}>
-              <FormUserDriver />
-            </WrapperSection>
-          </Col>
-        </Row>
-      </MainLayout>
-    )
+  return (
+    <MainLayout title='Profile'>
+      <Row display='flex' justify='center'>
+        <SideNav currentLocation='0' />
+        <Col span={20}>
+          <WrapperSection styles={stylesWrapper} row={20} mt={0}>
+            <FormUserDriver />
+          </WrapperSection>
+        </Col>
+      </Row>
+    </MainLayout>
+  )
 };
 
 export default DriverProfileView;
