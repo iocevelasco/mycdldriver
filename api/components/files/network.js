ab1016611f271dbfa30b05707b8a567399cf4d9e
Erick <takashi.onimaru@gmail.com>
2021-01-07 21:52:39 -0400

    Subida de imagenes en S3 AWS

--- api/components/files/network.js
+++ api/components/files/network.js
@@ -5,37 +5,65 @@ const router = express.Router();
 const response = require('../../network/response');
 const config = require('../../config');
 const controller = require('../user/controller');
+const aws = require('aws-sdk');
 
 const fs = require('fs');
 
+aws.config.setPromisesDependency();
+aws.config.update({
+  accessKeyId: config.s3.accessKeyId,
+  secretAccessKey: config.s3.secretAccessKey,
+  region: config.s3.region
+});
+const s3 = new aws.S3();
+
 router.post('/', storage.single('logo'), function (req, res) {
     const logo = req.file;
-    if(logo){
-        const fileUrl = logo ? config.publicRoute + config.filesRoute + '/' + logo.filename : '';
-        try{
-            const token = req.header('Authorization').replace('Bearer ', '');
-            const data = jwt.verify(token, config.JWT_KEY);
-            if(data._id){
-                controller.setPhoto(data._id, fileUrl)
-                .then((data) => {
-                    response.success(req, res, {'file': fileUrl}, 201);
-                })
-                .catch(e => {
-                    response.error(req, res, 'Error interno', 500, e);
-                });
-            }
-        }catch(e){
-            console.log(e);
+
+    var params = {
+        ACL: 'public-read',
+        Bucket: config.s3.bucketName,
+        Body: fs.createReadStream(req.file.path),
+        Key: `${req.file.filename}`
+    };
+
+    s3.upload(params, (err, data) => {
+        if (err) {
+            response.error(req, res, 'Error occured while trying to upload to S3 bucket', 500, err);
         }
-        response.success(req, res, {'file': fileUrl}, 201);
-    }else{
-        response.error(req, res, 'informacion invalida', 500);
-    }
+        if (data) {
+            fs.unlinkSync(req.file.path);
+            const locationUrl = data.Location;
+            try{
+                const token = req.header('Authorization').replace('Bearer ', '');
+                const data = jwt.verify(token, config.JWT_KEY);
+                if(data._id){
+                    controller.setPhoto(data._id, locationUrl)
+                    .then((data) => {
+                        response.success(req, res, {'file': locationUrl}, 201);
+                    })
+                    .catch(e => {
+                        response.error(req, res, 'Error interno', 500, e);
+                    });
+                }
+            }catch(e){
+                console.log(e);
+            }
+            response.success(req, res, {'file': locationUrl}, 201);
+        }
+    });
 });
 
 router.post('/delete', function (req, res) {
     const foto = req.body.foto;
-    console.log(foto);
+    const params = { 
+        Bucket: config.s3.bucketName, 
+        Key: foto 
+    };
+    s3.deleteObject(params, function(err, data) {
+        if (err) console.log(err, err.stack);  // error
+        else     console.log('Borrado');                 // deleted
+    });
     try{
         fs.unlinkSync("." + foto);
         response.success(req, res, "delete success", 201);
