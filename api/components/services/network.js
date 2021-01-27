976d3cfe44c01ad738e089a67c255ddf872a5022
Erick <takashi.onimaru@gmail.com>
2021-01-26 20:56:31 -0400

    Nuevo CRUD de servicios

---
+++ api/components/services/network.js
@@ -0,0 +1,88 @@
+const express = require('express');
+const auth = require('../../middelware/auth');
+const router = express.Router();
+const response = require('../../network/response');
+const controller = require('./controller');
+
+router.post('/', auth(2), function (req, res) {
+    controller.setService(req.body, req.user)
+      .then((service) => {
+        switch (service.status) {
+          case 201:
+            response.success(req, res, service.message, 201);
+            break;
+          default:
+            response.error(req, res, service.message, service.status);
+        }
+  
+      }).catch(e => {
+          console.log(e);
+        response.error(req, res, 'Unexpected network Error', 500, e);
+      });
+});
+
+router.get('/:id', function (req, res) {
+    controller.getServices(req.params.id)
+      .then((service) => {
+        switch (service.status) {
+          case 200:
+            response.success(req, res, service.message, 200);
+            break;
+          default:
+            response.error(req, res, service.message, service.status);
+        }
+  
+      }).catch(e => {
+        response.error(req, res, 'Unexpected network Error', 500, e);
+      });
+});
+
+router.get('/find/:id', function (req, res) {
+    controller.getService(req.params.id)
+      .then((service) => {
+        switch (service.status) {
+          case 200:
+            response.success(req, res, service.message, 200);
+            break;
+          default:
+            response.error(req, res, service.message, service.status);
+        }
+  
+      }).catch(e => {
+        response.error(req, res, 'Unexpected network Error', 500, e);
+      });
+});
+
+router.patch('/', auth(2), function (req, res) {
+    controller.updateService(req.body)
+      .then((service) => {
+        switch (service.status) {
+          case 200:
+            response.success(req, res, service.message, 200);
+            break;
+          default:
+            response.error(req, res, service.message, service.status);
+        }
+  
+      }).catch(e => {
+        response.error(req, res, 'Unexpected network Error', 500, e);
+      });
+});
+
+router.delete('/:id', auth(2), function (req, res) {
+  controller.deleteService(req.params.id)
+    .then((service) => {
+      switch (service.status) {
+        case 200:
+          response.success(req, res, service.message, 200);
+          break;
+        default:
+          response.error(req, res, service.message, service.status);
+          break;
+      }
+    }).catch(e => {
+      response.error(req, res, 'Unexpected network Error', 500, e);
+    });
+});
+
+module.exports = router;
---
+++ api/components/services/store.js
@@ -0,0 +1,152 @@
+const Services = require('./model');
+
+async function getServices(companyId) {
+    try {
+      if (!companyId) {
+        return {
+          status: 400,
+          message: 'No company id recived'
+        };
+      }
+      const query = {
+        company: companyId
+      }
+  
+      const result = await Services.find(query).populate("company");
+      return { status: 200, message: result }
+    } catch (e) {
+      return {
+        status: 500,
+        message: 'Unexpected store error',
+        detail: e
+      };
+    } 
+}
+
+async function getService(serviceId) {
+    try {
+      if (!serviceId) {
+        return {
+          status: 400,
+          message: 'No service id recived'
+        };
+      }
+      const query = {
+        _id: serviceId
+      }
+  
+      const result = await Services.findOne(query).populate("company");
+      return { status: 200, message: result }
+    } catch (e) {
+      return {
+        status: 500,
+        message: 'Unexpected store error',
+        detail: e
+      };
+    } 
+}
+
+async function setService(service, company) {
+
+    try {
+      if (!service) {
+        return {
+          status: 400,
+          message: 'No service recived'
+        };
+      }
+      if (!company) {
+        return {
+          status: 400,
+          message: 'No company recived'
+        };
+      }
+      service.company = company._id;
+      const serviceModel = new Services(service)
+      const serviceResult = await serviceModel.save();
+  
+      return { status: 201, message: serviceResult };
+    } catch (e) {
+      return {
+        status: 500,
+        message: 'Unexpected store error',
+        detail: e
+      };
+    }
+}
+
+async function updateService(service){
+    try{
+        const foundService = await Services.findOne({
+            _id: service.id
+        });
+
+        if(service.title){
+            foundService.title = service.title;
+        }
+        if(service.detail){
+            foundService.detail = service.detail;
+        }
+        if(service.image){
+            if(foundService.image != service.image){
+                foundService.image = service.image;
+            }
+        }
+        if(service.email){
+            foundService.email = service.email;
+        }
+        if(service.phone){
+            foundService.phone = service.phone;
+        }
+        if(service.whatsapp){
+            foundService.whatsapp = service.whatsapp;
+        }
+        if(service.includeService){
+            foundService.includeService = service.includeService;
+        }
+        if(service.state){
+            foundService.state = service.state;
+        }
+        if(service.city){
+            foundService.city = service.city;
+        }
+
+        const serviceResult = await foundService.save();
+        return { status: 200, message: serviceResult };
+
+    }catch(e){
+        return {
+            status: 500,
+            message: 'Unexpected store error',
+            detail: e
+        };
+    }
+}
+
+async function deleteService(id) {
+  if(!id){
+    return {
+      status: 400,
+      message: 'No service id recived'
+    };
+  }
+
+  try{
+    await Services.findOneAndDelete({_id: id});
+    return { status: 200, message: 'The service has been deleted correctly' }
+  }catch(e){
+    return {
+      status: 500,
+      message: 'Unexpected error',
+      detail: e
+    };
+  }
+}
+
+module.exports = {
+    getServices,
+    getService,
+    setService,
+    updateService,
+    deleteService
+}
