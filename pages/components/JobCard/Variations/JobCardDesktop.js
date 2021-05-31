9e52fc4906031b16874460d968d859d337568e4c
Joaquin Larralde <joaquinlarralde@Joaquins-MacBook-Pro.local>
2021-05-31 09:29:24 -0300

    feat: mobile style

--- pages/components/JobCard/Variations/JobCardDesktop.js
+++ pages/components/JobCard/Variations/JobCardDesktop.js
@@ -1,12 +1,10 @@
-import React from 'react';
-import { withRouter } from 'next/router';
-import { Card, Avatar, Typography, Button } from 'antd';
-import moment from 'moment';
-import Link from 'next/link';
+import React from "react";
+import { withRouter } from "next/router";
+import { Card, Avatar, Typography, Button } from "antd";
+import moment from "moment";
+import Link from "next/link";
 
-
-
-const { Title, Text } = Typography
+const { Title, Text } = Typography;
 
 const JobCardDesktop = (props) => {
   const { item } = props;
@@ -18,7 +16,10 @@ const JobCardDesktop = (props) => {
         <>
           <div className="card-job__header__company">
             <Avatar shape="square" size={120} src={item.company.photo} />
-            <p className="card-job__description__title" level={3}> {item.company.tradename} </p>
+            <p className="card-job__description__title" level={3}>
+              {" "}
+              {item.company.tradename}{" "}
+            </p>
           </div>
           <img alt="logo" src={item.logo} />
         </>
@@ -26,19 +27,22 @@ const JobCardDesktop = (props) => {
     >
       <Link
         href={{
-          pathname: '/job-offert',
+          pathname: "/job-offert",
           query: { id: item._id },
         }}
       >
         <div className="card-job__container">
           <div className="card-job__header">
-            <div className="card-job__header__title" >
+            <div className="card-job__header__title">
               <Title level={4}> {item.title} </Title>
               <div className="card-job__header__sub-title">
                 <Text>
-                  {item.company.address}  |  {item.company.address2}
+                  {item.company.address} | {item.company.address2}
                 </Text>
-                <Text type="secondary" > {moment(item.date, "YYYYMMDD").fromNow()}</Text>
+                <Text type="secondary">
+                  {" "}
+                  {moment(item.date, "YYYYMMDD").fromNow()}
+                </Text>
               </div>
             </div>
           </div>
@@ -46,19 +50,20 @@ const JobCardDesktop = (props) => {
             <div>
               <p> {item.description} </p>
             </div>
-            {
-              props.type !== 'small' ?
-                <Button type="primary" shape="round" style={{ width: "140px" }}>Aply now</Button>
-                :
-                <Button type="primary" shape="round" style={{ width: "140px" }}>View more</Button>
-            }
+            {props.type !== "small" ? (
+              <Button type="primary" shape="round" style={{ width: "140px" }}>
+                Apply now
+              </Button>
+            ) : (
+              <Button type="primary" shape="round" style={{ width: "140px" }}>
+                View more
+              </Button>
+            )}
           </div>
         </div>
       </Link>
-    </Card >
-  )
-}
-
-
+    </Card>
+  );
+};
 
 export default withRouter(JobCardDesktop);
--- pages/home/components/driver_list.js
+++ pages/home/components/driver_list.js
@@ -1,17 +1,16 @@
 import React, { useRef } from "react";
 import { Button, Col } from "antd";
-import { CardDriver } from '@components/Cards'
-import useMobileDetect from 'use-mobile-detect-hook';
+import { CardDriver } from "@components/Cards";
+import useMobileDetect from "use-mobile-detect-hook";
 import { withRouter } from "next/router";
-import Link from 'next/link';
+import Link from "next/link";
 import { Carousel } from "antd";
 
-
 const DriverListSection = (props) => {
-  console.log('DriverListSection', props)
+  console.log("DriverListSection", props);
   const handleSelect = (id) => {
-    console.log('d', id);
-  }
+    console.log("d", id);
+  };
   const detectMobile = useMobileDetect();
   const slider = useRef();
 
@@ -26,35 +25,37 @@ const DriverListSection = (props) => {
           slider.current = ref;
         }}
       >
-        {
-          props.driversList.map((data, key) => {
-            const city = data.driver.city ? data.driver.city.cityName : "";
-            const state = data.driver.state ? data.driver.state.stateName : "";
-            return (
-              <Col >
-                <div key={key} className="home__driver-list--carousel-list">
-                  <CardDriver
-                    bordered={false}
-                    handlerAction={() => handleSelect(data)}
-                    city={city}
-                    state={state}
-                    fullName={`${data.name} ${data.lastname}`}
-                    rating={data.driver.rating}
-                    photo={data.photo}
-                    darkTheme={true}
-                  />
-                </div>
-              </Col>
-            );
-          })}
+        {props.driversList.map((data, key) => {
+          const city = data.driver.city ? data.driver.city.cityName : "";
+          const state = data.driver.state ? data.driver.state.stateName : "";
+          return (
+            <Col key={key}>
+              <div className="home__driver-list--carousel-list">
+                <CardDriver
+                  bordered={false}
+                  handlerAction={() => handleSelect(data)}
+                  city={city}
+                  state={state}
+                  fullName={`${data.name} ${data.lastname}`}
+                  rating={data.driver.rating}
+                  photo={data.photo}
+                  darkTheme={true}
+                />
+              </div>
+            </Col>
+          );
+        })}
       </Carousel>
-      {
-        props.typeUser === 2 ? <div className="home__driver-list--action-container">
-          <Link href='/list-drivers'>
-            <Button shape="round" ghost size='large'> View all </Button>
+      {props.typeUser === 2 ? (
+        <div className="home__driver-list--action-container">
+          <Link href="/list-drivers">
+            <Button shape="round" ghost size="large">
+              {" "}
+              View all{" "}
+            </Button>
           </Link>
-        </div> : null
-      }
+        </div>
+      ) : null}
     </div>
   );
 };
