886a54df6d23214e9fc5769f9cb88190f2451ed0
Erick <takashi.onimaru@gmail.com>
2021-06-16 17:47:54 -0400

    Carrusel en Home (Job & Service)

--- pages/home/components/services_list.js
+++ pages/home/components/services_list.js
@@ -1,8 +1,9 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { fetchServices } from "@store/reducers/landing_reducer";
-import { Row, Col } from "antd";
+import { Row, Col, Carousel } from "antd";
 import CardServices from "components/CardServices";
+import useMobileDetect from 'use-mobile-detect-hook';
 
 function mapStateToProps(state) {
   return {
@@ -12,29 +13,53 @@ function mapStateToProps(state) {
 }
 
 const ServicesList = (props) => {
+  const detectMobile = useMobileDetect();
   const { servicesArray } = props;
 
   return (
-    <Row gutter={[24, 24]}>
-      {servicesArray.map((service, key) => {
-        return (
-          <Col key={key} lg={12} md={12} xs={24}>
-            <CardServices
-              type="home"
-              edit={false}
-              image={service.image}
-              city={service.city}
-              email={service.email}
-              title={service.title}
-              whatsapp={service.whatsapp}
-              _id={service._id}
-              detail={service.detail}
-              state={service.state}
-            />
-          </Col>
-        );
-      })}
-    </Row>
+    <div>
+      {detectMobile.isMobile() ? 
+        <Carousel autoplay={true} dots={false}>
+          {servicesArray.map((service, key) => {
+            return (
+              <Col key={key} span={24}>
+                <CardServices
+                  type="home"
+                  edit={false}
+                  image={service.image}
+                  city={service.city}
+                  email={service.email}
+                  title={service.title}
+                  whatsapp={service.whatsapp}
+                  _id={service._id}
+                  detail={service.detail}
+                  state={service.state}
+                />
+              </Col>
+            );
+          })}
+        </Carousel> :
+        <Row gutter={[24, 24]}>
+        {servicesArray.map((service, key) => {
+          return (
+            <Col key={key} lg={12} md={12} xs={24}>
+              <CardServices
+                type="home"
+                edit={false}
+                image={service.image}
+                city={service.city}
+                email={service.email}
+                title={service.title}
+                whatsapp={service.whatsapp}
+                _id={service._id}
+                detail={service.detail}
+                state={service.state}
+              />
+            </Col>
+          );
+        })}
+        </Row>
+      }</div>
   );
 };
 
