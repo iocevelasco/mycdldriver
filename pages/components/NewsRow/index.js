7695623b08ec5fa9545c5453b56a299f4b9ca200
Erick <takashi.onimaru@gmail.com>
2021-08-10 12:35:57 -0400

    Ajuste visual en detalle new y quitada importacion fallida en estilo

--- pages/components/NewsRow/index.js
+++ pages/components/NewsRow/index.js
@@ -13,17 +13,11 @@ const NewsRow = (props) => {
         <Row justify='center'>
             <Col xs={22} lg={20}>
                 <div className="news-details__top">
-                <div className="news-details__top--title">
-                    <Title level={2}> 
-                        {notice.title}
-                    </Title>
-                </div>
-                <div className="news-details__top--summary">
-                    <Text>
-                        {notice.description}
-                    </Text>
-                    <p style={{fontSize: 13, marginTop: 5}}>17/09/2021</p>
-                </div>
+                    <div className="news-details__top--title">
+                        <Title level={2}> 
+                            {notice.title}
+                        </Title>
+                    </div>
                 </div>
             </Col>
             <Col xs={22} lg={20}>
@@ -34,11 +28,6 @@ const NewsRow = (props) => {
             <Col xs={22} lg={20}>
                 <div  className='news_details-description'>
                     {notice.description}
-                </div>
-            </Col>
-            <Col xs={22} lg={18}>
-                <div className="news-details__img" align="center">
-                  <Image height={'100%'} src={notice.image}></Image>
                 </div>
             </Col>
         </Row>
